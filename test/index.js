24d896ddf43765205b01a0b8775bb2214693fa23
u9g <43508353+u9g@users.noreply.github.com>
2021-01-28 04:13:45 -0500

    made tests use arrow functions (#55)
    * made tests use arrow functions
    
    * fixed tests failing !
    
    * fixed tests failing
    
    also added back the comment for es-lint
    
    * I shouldn't have actually removed that lol

--- test/index.js
+++ test/index.js
@@ -7,26 +7,26 @@ const nock = require('nock')
 
 const utils = require('../lib/utils')
 
-describe('utils', function () {
-  describe('call', function () {
+describe('utils', () => {
+  describe('call', () => {
     const google = 'https://google.com'
     const uscope = nock(google)
 
-    it('should work when given valid data', function (done) {
+    it('should work when given valid data', done => {
       const bsdata = {
         cake: true,
         username: 'someone'
       }
 
       uscope.post('/test', {}).reply(200, bsdata)
-      utils.call(google, 'test', {}, undefined, function (err, data) {
+      utils.call(google, 'test', {}, undefined, (err, data) => {
         assert.ifError(err)
         assert.deepStrictEqual(data, bsdata)
         done()
       })
     })
 
-    it('should work when given valid data (promise)', async function () {
+    it('should work when given valid data (promise)', async () => {
       const bsdata = {
         cake: true,
         username: 'someone'
@@ -37,19 +37,19 @@ describe('utils', function () {
       assert.deepStrictEqual(data, bsdata)
     })
 
-    it('should error on an error', function (done) {
+    it('should error on an error', done => {
       uscope.post('/test2', {}).reply(200, {
         error: 'ThisBeAError',
         errorMessage: 'Yep, you failed.'
       })
-      utils.call(google, 'test2', {}, undefined, function (err, data) {
+      utils.call(google, 'test2', {}, undefined, (err, data) => {
         assert.strictEqual(data, undefined)
         assert.ok(err instanceof Error)
         assert.strictEqual(err.message, 'Yep, you failed.')
         done()
       })
     })
-    it('should error on an error (promise)', async function () {
+    it('should error on an error (promise)', async () => {
       uscope.post('/test2', {}).reply(200, {
         error: 'ThisBeAError',
         errorMessage: 'Yep, you failed.'
@@ -62,14 +62,14 @@ describe('utils', function () {
       }
     })
 
-    afterEach(function () {
+    afterEach(() => {
       uscope.done()
     })
   })
 
   // mcHexDigest(sha1('catcatcat')) => -af59e5b1d5d92e5c2c2776ed0e65e90be181f2a
-  describe('mcHexDigest', function () {
-    it('should work against test data', function () {
+  describe('mcHexDigest', () => {
+    it('should work against test data', () => {
       // circa http://wiki.vg/Protocol_Encryption#Client
       const testdata = {
         Notch: '4ed1f46bbe04bc756bcb17c0c7ce3e4632f06a48',
@@ -78,13 +78,13 @@ describe('utils', function () {
         dummy697: '-aa2358520428804697026992cf6035d7f096a00' // triggers 2's complement bug
       }
 
-      Object.keys(testdata).forEach(function (name) {
+      Object.keys(testdata).forEach(name => {
         const hash = crypto.createHash('sha1').update(name).digest()
         assert.strictEqual(utils.mcHexDigest(hash), testdata[name])
       })
     })
 
-    it('should handle negative hashes ending with a zero byte without crashing', function () {
+    it('should handle negative hashes ending with a zero byte without crashing', () => {
       assert.strictEqual(utils.mcHexDigest(Buffer.from([-1, 0])), '-100')
     })
   })
@@ -93,9 +93,9 @@ describe('utils', function () {
 const cscope = nock('https://authserver.mojang.com')
 const ygg = require('../lib/index')({})
 
-describe('Yggdrasil', function () {
-  describe('auth', function () {
-    it('should work correctly', function (done) {
+describe('Yggdrasil', () => {
+  describe('auth', () => {
+    it('should work correctly', done => {
       cscope.post('/authenticate', {
         agent: {
           version: 1,
@@ -112,7 +112,7 @@ describe('Yggdrasil', function () {
         user: 'cake',
         pass: 'hunter2',
         token: 'bacon'
-      }, function (err, data) { // eslint-disable-line handle-callback-err
+      }, (err, data) => { // eslint-disable-line handle-callback-err
         if (err) {
           done(err)
           return
@@ -123,7 +123,7 @@ describe('Yggdrasil', function () {
         done()
       })
     })
-    it('should work correctly (promise)', async function () {
+    it('should work correctly (promise)', async () => {
       cscope.post('/authenticate', {
         agent: {
           version: 1,
@@ -139,7 +139,7 @@ describe('Yggdrasil', function () {
       const data = await ygg.auth({ user: 'cake', pass: 'hunter2', token: 'bacon' })
       assert.deepStrictEqual(data, { worked: true })
     })
-    it('should work correctly with requestUser true', function (done) {
+    it('should work correctly with requestUser true', done => {
       cscope.post('/authenticate', {
         agent: {
           version: 1,
@@ -157,7 +157,7 @@ describe('Yggdrasil', function () {
         pass: 'hunter2',
         token: 'bacon',
         requestUser: true
-      }, function (err, data) { // eslint-disable-line handle-callback-err
+      }, (err, data) => { // eslint-disable-line handle-callback-err
         if (err) {
           done(err)
           return
@@ -168,7 +168,7 @@ describe('Yggdrasil', function () {
         done()
       })
     })
-    it('should work correctly with requestUser true (promise)', async function () {
+    it('should work correctly with requestUser true (promise)', async () => {
       cscope.post('/authenticate', {
         agent: {
           version: 1,
@@ -185,8 +185,8 @@ describe('Yggdrasil', function () {
       assert.deepStrictEqual(data, { worked: true })
     })
   })
-  describe('refresh', function () {
-    it('should work correctly', function (done) {
+  describe('refresh', () => {
+    it('should work correctly', done => {
       cscope.post('/refresh', {
         accessToken: 'bacon',
         clientToken: 'not bacon',
@@ -195,13 +195,13 @@ describe('Yggdrasil', function () {
         accessToken: 'different bacon',
         clientToken: 'not bacon'
       })
-      ygg.refresh('bacon', 'not bacon', function (err, token) {
+      ygg.refresh('bacon', 'not bacon', (err, token) => {
         assert.ifError(err)
         assert.strictEqual(token, 'different bacon')
         done()
       })
     })
-    it('should work correctly (promise)', async function () {
+    it('should work correctly (promise)', async () => {
       cscope.post('/refresh', {
         accessToken: 'bacon',
         clientToken: 'not bacon',
@@ -213,7 +213,7 @@ describe('Yggdrasil', function () {
       const { accessToken } = await ygg.refresh('bacon', 'not bacon')
       assert.strictEqual(accessToken, 'different bacon')
     })
-    it('should work correctly with requestUser true', function (done) {
+    it('should work correctly with requestUser true', done => {
       cscope.post('/refresh', {
         accessToken: 'bacon',
         clientToken: 'not bacon',
@@ -226,7 +226,7 @@ describe('Yggdrasil', function () {
           properties: []
         }
       })
-      ygg.refresh('bacon', 'not bacon', true, function (err, token, data) {
+      ygg.refresh('bacon', 'not bacon', true, (err, token, data) => {
         assert.ifError(err)
         assert.strictEqual(token, 'different bacon')
         assert.ok(data.user)
@@ -235,7 +235,7 @@ describe('Yggdrasil', function () {
         done()
       })
     })
-    it('should work correctly with requestUser true (promise)', async function () {
+    it('should work correctly with requestUser true (promise)', async () => {
       cscope.post('/refresh', {
         accessToken: 'bacon',
         clientToken: 'not bacon',
@@ -254,7 +254,7 @@ describe('Yggdrasil', function () {
       assert.ok(data.user.properties)
       assert.strictEqual(data.user.id, '4ed1f46bbe04bc756bcb17c0c7ce3e4632f06a48')
     })
-    it('should error on invalid clientToken', function (done) {
+    it('should error on invalid clientToken', done => {
       cscope.post('/refresh', {
         accessToken: 'bacon',
         clientToken: 'not bacon',
@@ -263,14 +263,14 @@ describe('Yggdrasil', function () {
         accessToken: 'different bacon',
         clientToken: 'bacon'
       })
-      ygg.refresh('bacon', 'not bacon', function (err, token) {
+      ygg.refresh('bacon', 'not bacon', (err, token) => {
         assert.notStrictEqual(err, null)
         assert.ok(err instanceof Error)
         assert.strictEqual(err.message, 'clientToken assertion failed')
         done()
       })
     })
-    it('should error on invalid clientToken (promise)', async function () {
+    it('should error on invalid clientToken (promise)', async () => {
       cscope.post('/refresh', {
         accessToken: 'bacon',
         clientToken: 'not bacon',
@@ -288,36 +288,36 @@ describe('Yggdrasil', function () {
       }
     })
   })
-  describe('validate', function () {
-    it('should return undefined on valid response', function (done) {
+  describe('validate', () => {
+    it('should return undefined on valid response', done => {
       cscope.post('/validate', {
         accessToken: 'a magical key'
       }).reply(200)
-      ygg.validate('a magical key', function (err) {
+      ygg.validate('a magical key', err => {
         assert.ifError(err)
         done()
       })
     })
-    it('should return undefined on valid response (promise)', async function () {
+    it('should return undefined on valid response (promise)', async () => {
       cscope.post('/validate', {
         accessToken: 'a magical key'
       }).reply(200)
       await ygg.validate('a magical key')
     })
-    it('should return Error on error', function (done) {
+    it('should return Error on error', done => {
       cscope.post('/validate', {
         accessToken: 'a magical key'
       }).reply(403, {
         error: 'UserEggError',
         errorMessage: 'User is an egg'
       })
-      ygg.validate('a magical key', function (err) {
+      ygg.validate('a magical key', err => {
         assert.ok(err instanceof Error)
         assert.strictEqual(err.message, 'User is an egg')
         done()
       })
     })
-    it('should return Error on error (promise)', async function () {
+    it('should return Error on error (promise)', async () => {
       cscope.post('/validate', {
         accessToken: 'a magical key'
       }).reply(403, {
@@ -332,7 +332,7 @@ describe('Yggdrasil', function () {
       }
     })
   })
-  afterEach(function () {
+  afterEach(() => {
     cscope.done()
   })
 })
@@ -340,9 +340,9 @@ describe('Yggdrasil', function () {
 const sscope = nock('https://sessionserver.mojang.com')
 const yggserver = require('../lib/index').server({})
 
-describe('Yggdrasil.server', function () {
-  describe('join', function () {
-    it('should work correctly', function (done) {
+describe('Yggdrasil.server', () => {
+  describe('join', () => {
+    it('should work correctly', done => {
       sscope.post('/session/minecraft/join', {
         accessToken: 'anAccessToken',
         selectedProfile: 'aSelectedProfile',
@@ -351,7 +351,7 @@ describe('Yggdrasil.server', function () {
         worked: true
       })
 
-      yggserver.join('anAccessToken', 'aSelectedProfile', 'cat', 'cat', 'cat', function (err, data) { // eslint-disable-line handle-callback-err
+      yggserver.join('anAccessToken', 'aSelectedProfile', 'cat', 'cat', 'cat', (err, data) => { // eslint-disable-line handle-callback-err
         if (err) {
           done(err)
           return
@@ -362,7 +362,7 @@ describe('Yggdrasil.server', function () {
         done()
       })
     })
-    it('should work correctly (promise)', async function () {
+    it('should work correctly (promise)', async () => {
       sscope.post('/session/minecraft/join', {
         accessToken: 'anAccessToken',
         selectedProfile: 'aSelectedProfile',
@@ -375,14 +375,14 @@ describe('Yggdrasil.server', function () {
     })
   })
 
-  describe('hasJoined', function () {
-    it('should work correctly', function (done) {
+  describe('hasJoined', () => {
+    it('should work correctly', done => {
       sscope.get('/session/minecraft/hasJoined?username=ausername&serverId=-af59e5b1d5d92e5c2c2776ed0e65e90be181f2a').reply(200, {
         id: 'cat',
         worked: true
       })
 
-      yggserver.hasJoined('ausername', 'cat', 'cat', 'cat', function (err, data) {
+      yggserver.hasJoined('ausername', 'cat', 'cat', 'cat', (err, data) => {
         if (err) return done(err)
         assert.deepStrictEqual(data, {
           id: 'cat',
@@ -391,7 +391,7 @@ describe('Yggdrasil.server', function () {
         done()
       })
     })
-    it('should work correctly (promise)', async function () {
+    it('should work correctly (promise)', async () => {
       sscope.get('/session/minecraft/hasJoined?username=ausername&serverId=-af59e5b1d5d92e5c2c2776ed0e65e90be181f2a').reply(200, {
         id: 'cat',
         worked: true
@@ -403,15 +403,15 @@ describe('Yggdrasil.server', function () {
         worked: true
       })
     })
-    it('should fail on a 200 empty response', function (done) {
+    it('should fail on a 200 empty response', done => {
       sscope.get('/session/minecraft/hasJoined?username=ausername&serverId=-af59e5b1d5d92e5c2c2776ed0e65e90be181f2a').reply(200)
 
-      yggserver.hasJoined('ausername', 'cat', 'cat', 'cat', function (err, data) {
+      yggserver.hasJoined('ausername', 'cat', 'cat', 'cat', (err, data) => {
         assert.ok(err instanceof Error)
         done()
       })
     })
-    it('should fail on a 200 empty response (promise)', async function () {
+    it('should fail on a 200 empty response (promise)', async () => {
       sscope.get('/session/minecraft/hasJoined?username=ausername&serverId=-af59e5b1d5d92e5c2c2776ed0e65e90be181f2a').reply(200)
       try {
         await yggserver.hasJoined('ausername', 'cat', 'cat', 'cat')
@@ -420,7 +420,7 @@ describe('Yggdrasil.server', function () {
       }
     })
   })
-  afterEach(function () {
+  afterEach(() => {
     sscope.done()
   })
 })
